499e98716de05f8c2ce5951aa0acdd9784b7b81c
Brad Broge <brbroge@gmail.com>
2017-10-23 15:42:50 -0400

    getting the react app organized into workable chunks

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+import applicantForm from './Form';
+import applicantResponse from './Responses'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
